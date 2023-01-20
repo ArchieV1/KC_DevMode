28fa6a9c631a3b274f71ffb740171b5b706c61da
ArchieV1 <archie.j.vann@gmail.com>
2023-01-20 01:58:22 +0000

    Removed update news from menu screen

--- DevMode.cs
+++ DevMode.cs
@@ -45,8 +45,9 @@ namespace KC_DevMode
         
         public void Start()
         {
-            MainMenuMode.topLevelUICanvas;
-            string test;
+            // Delete update news
+            Destroy(GameObject.Find("UpdateContainer"));
+            Destroy(GameObject.Find("UpdateDescription"));
         }
 
         /// <summary>
@@ -107,7 +108,7 @@ namespace KC_DevMode
  
         public void AddEvent(string modName, string msg)
         {
-            Eventlog.Add($"[{modName.Substring(0, Math.Min(15, modName.Length)), -15} | {DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}] {msg.Substring(0, Math.Min(40, msg.Length)), -40}");
+            Eventlog.Add($"[{modName.Substring(0, Math.Min(15, modName.Length)), -15} | {DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}] {msg.Substring(0, Math.Min(70, msg.Length)), -70}");
             
             GuiText = "";
  
@@ -131,15 +132,33 @@ namespace KC_DevMode
         }
     }
 
-    [HarmonyPatch(typeof(Debug), nameof(Debug.Log))]
-    class Patch_Debug
-    {
-        static void Postfix(string msg)
-        {
-            DevMode.inst.AddEvent("DEBUG", msg);
-            // DevMode.inst.RuntimeLogs.Add(msg);
-        }
-    }
+    // [HarmonyPatch(typeof(Debug), nameof(Debug.Log))]
+    // class Patch_Debug
+    // {
+    //     static void Postfix(string msg)
+    //     {
+    //         DevMode.inst.AddEvent("DEBUG", msg);
+    //         // DevMode.inst.RuntimeLogs.Add(msg);
+    //     }
+    // }
+  //   2023-01-19T23:24:15:System.Reflection.TargetInvocationException: Exception has been thrown by the target of an invocation. ---> System.Reflection.AmbiguousMatchException: Ambiguous match found.
+  // at System.RuntimeType.GetMethodImpl (System.String name, System.Reflection.BindingFlags bindingAttr, System.Reflection.Binder binder, System.Reflection.CallingConventions callConv, System.Type[] types, System.Reflection.ParameterModifier[] modifiers) [0x00059] in <eae584ce26bc40229c1b1aa476bfa589>:0 
+  // at System.Type.GetMethod (System.String name, System.Reflection.BindingFlags bindingAttr) [0x0000e] in <eae584ce26bc40229c1b1aa476bfa589>:0 
+  // at Harmony.AccessTools.DeclaredMethod (System.Type type, System.String name, System.Type[] parameters, System.Type[] generics) [0x0002a] in <ef472ad41f4948fdb52674edfe069979>:0 
+  // at Harmony.PatchProcessor.GetOriginalMethod () [0x00075] in <ef472ad41f4948fdb52674edfe069979>:0 
+  // at Harmony.PatchProcessor.PrepareType () [0x00101] in <ef472ad41f4948fdb52674edfe069979>:0 
+  // at Harmony.PatchProcessor..ctor (Harmony.HarmonyInstance instance, System.Type type, Harmony.HarmonyMethod attributes) [0x00065] in <ef472ad41f4948fdb52674edfe069979>:0 
+  // at Harmony.HarmonyInstance.<PatchAll>b__9_0 (System.Type type) [0x00023] in <ef472ad41f4948fdb52674edfe069979>:0 
+  // at Harmony.CollectionExtensions.Do[T] (System.Collections.Generic.IEnumerable`1[T] sequence, System.Action`1[T] action) [0x0001b] in <ef472ad41f4948fdb52674edfe069979>:0 
+  // at Harmony.HarmonyInstance.PatchAll (System.Reflection.Assembly assembly) [0x00007] in <ef472ad41f4948fdb52674edfe069979>:0 
+  // at KC_DevMode.DevMode.Preload (KCModHelper helper) [0x00018] in <9dbe81d97a0c4405bfb6a5ba893c1acd>:0 
+  // at (wrapper managed-to-native) System.Reflection.MonoMethod.InternalInvoke(System.Reflection.MonoMethod,object,object[],System.Exception&)
+  // at System.Reflection.MonoMethod.Invoke (System.Object obj, System.Reflection.BindingFlags invokeAttr, System.Reflection.Binder binder, System.Object[] parameters, System.Globalization.CultureInfo culture) [0x00032] in <eae584ce26bc40229c1b1aa476bfa589>:0 
+  //  --- End of inner exception stack trace ---
+  // at System.Reflection.MonoMethod.Invoke (System.Object obj, System.Reflection.BindingFlags invokeAttr, System.Reflection.Binder binder, System.Object[] parameters, System.Globalization.CultureInfo culture) [0x00048] in <eae584ce26bc40229c1b1aa476bfa589>:0 
+  // at System.Reflection.MethodBase.Invoke (System.Object obj, System.Object[] parameters) [0x00000] in <eae584ce26bc40229c1b1aa476bfa589>:0 
+  // at RoslynCSharp.ScriptProxy.Call (System.String methodName, RoslynCSharp.ProxyCallConvention callConvention, System.Object[] arguments) [0x0001a] in <94aa4123b58043a4a668862cd4ae830f>:0 
+  // at KCModHelper+ModLoader.SendSignal (System.String functionName) [0x0006e] in <4c75e12cdebf4ad08046fab68c3d730c>:0 
     
     
     public static class Tools{
